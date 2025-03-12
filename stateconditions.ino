62927394afcc5c81c1706e32dff6ca488bef0b6e
VIMistheonlyway <123780404+VIMistheonlyway@users.noreply.github.com>
2025-03-12 17:56:48 -0500

    Update stateconditions.ino

--- stateconditions.ino
+++ stateconditions.ino
@@ -5,7 +5,8 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); //figure out how to use LCD
 
-const float Vtrip = 3; //3V to trip, test to see level to trip
+const float Vtrip = 5; //5V to trip, test to see level to trip
+int resetflag = 0;
 
 void setup() {
   lcd.begin();
@@ -19,15 +20,16 @@ void loop() {
 //Voltage from current sensor in
 const float Vin = analogRead(A1); //value from the current sensor Andrew makes
 float voltage = Vin*(5/1024.0); 
-float tripped = voltage < Vtrip;
+float trip = voltage < Vtrip;
 
-if(tripped) {
+if(trip) {
   unsigned long trstartt = micros();
-  digitalWrite(2, LOW); //trips the line
+  digitalWrite(2, LOW); //trips the relay
   digitalWrite(4, HIGH); //turns on the trip system LED
 
   unsigned long trcurrentt = micros();
   unsigned long trtotalt = trcurrentt - trstartt;
+  resetflag = 1;
 
   lcd.clear();
   lcd.setCursor(1, 0); //overcurrent fault trip statement
@@ -39,7 +41,7 @@ if(tripped) {
   delay(10000);
 }
 
-else if(abs(voltage - Vtrip) < 0.1) {//change that value
+else if(abs(voltage - Vtrip) < 0.05 && resetflag == 1) {//checks the ripple of the voltage
   unsigned long rstartt = micros();
   digitalWrite(4, LOW); //turns off the trip system LED
   digitalWrite(2, HIGH); //resets the digital relay
@@ -61,7 +63,9 @@ else if(abs(voltage - Vtrip) < 0.1) {//change that value
   delay(10000);
 }
 
-if(!tripped) {
+if(resetflag == 0) {
+  resetflag = 0;
+  digitalWrite(2, HIGH); //digital relay  
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Normal Operation");
@@ -70,3 +74,4 @@ if(!tripped) {
 delay(1000);
 return;
 }
+
